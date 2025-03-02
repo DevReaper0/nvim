31e3bef03c974c48d4f44dc3a32bf1775d21234e
Robert Soncrant <devreaper0@gmail.com>
2025-03-02 16:36:42 -0600

    Add DAP for debugging

---
+++ lua/devreaper0/plugins/dap.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      handlers = {},
+    },
+  },
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+    cond = not vim.g.vscode,
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      dapui.setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+    end,
+  },
+}
--- lua/devreaper0/plugins/lsp.lua
+++ lua/devreaper0/plugins/lsp.lua
@@ -1,23 +1,22 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = {
-      "williamboman/mason.nvim",
-      "neovim/nvim-lspconfig",
-      "saghen/blink.cmp",
-    },
-    config = function()
-      local capabilities = require('blink.cmp').get_lsp_capabilities()
-      local lspconfig = require('lspconfig')
-
-      require("mason").setup()
-      require("mason-lspconfig").setup({
-        handlers = {
-          function(server_name)
-            lspconfig[server_name].setup({ capabilities = capabilities })
-          end,
-        },
-      })
-    end,
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
+    "saghen/blink.cmp",
   },
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+    local lspconfig = require('lspconfig')
+
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      handlers = {
+        function(server_name)
+          lspconfig[server_name].setup({ capabilities = capabilities })
+        end,
+      },
+    })
+  end,
 }
