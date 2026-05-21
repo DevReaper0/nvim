1e2163e68c1813935182caa8c99499ac27e6ac7d
Robert Soncrant <devreaper0@gmail.com>
2026-05-21 10:41:08 -0500

    Replace theme with Emberwood

--- lua/devreaper0/plugins/colors.lua
+++ lua/devreaper0/plugins/colors.lua
@@ -1,18 +1,17 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
+	"EmberwoodTheme/nvim",
+	name = "emberwood",
 	priority = 1000,
 	config = function()
-		local catppuccin = require("catppuccin")
-		catppuccin.flavours = { latte = 1, frappe = 2, macchiato = 3, mocha = 4, matrix = 5 }
-		catppuccin.setup({
-			flavour = "matrix",
-			show_end_of_buffer = true,
-			term_colors = true,
-			auto_integrations = true,
+		require("emberwood").setup({
+			ctp_options = {
+				show_end_of_buffer = true,
+				term_colors = true,
+				auto_integrations = true,
+			},
 		})
 
-		vim.cmd("colorscheme catppuccin-nvim")
+		vim.cmd("colorscheme emberwood")
 
 		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		vim.api.nvim_set_hl(0, "FloatTitle", { link = "Title" })
